f848d468a3e644f86a137b0e156713313a17f663
bigballon <605718199@qq.com>
2019-01-18 15:09:38 +0800

    add code and modify readme

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # pytorch_cpp
-C++ inference with libtorch
+
+This demo will show you how to use libtorch to build your C++ application.
+
+## Contents
+
+1. [Requirements](#requirements)
+2. [Build](#build)
+3. [Usage](#usage)
+
+
+## Requirements
+
+- Pytorch (tag: pytorch v1.0)
+- Libtorch
+- OpenCV
+
+## Build
+
+### Step 1
+
+Export your pytorch model to torch script file, We will simply use resnet50 in this demo
+
+### Step 2
+
+Write your C++ program, check the file ``prediction.cpp`` for more detial.  
+
+PS: ``module->to(at::kCUDA)`` and ``input_tensor.to(at::kCUDA)`` will switch your model & tensor to GPU mode,  
+comment out them if you just want to use CPU mode. 
+
+
+### Step 3
+
+Write a ``CMakeLists.txt``, the version of OpenCV must the same as your libtorch.
+Otherwise, you may get the compile error:
+
+```
+error: undefined reference to `cv::imread(std::string const&, int)'
+```
+
+check [issues 14684](https://github.com/pytorch/pytorch/issues/14684) and [issues 14620](https://github.com/pytorch/pytorch/issues/14620) for more details.
+
+## Usage
+
+- run ``model_trace.py``,   then you will get a file ``resnet50.pt``
+- compile your cpp program, you need to use ``-DCMAKE_PREFIX_PATH=/absolute/path/to/libtorch``, for example:
+
+```
+mkdir build
+cd build
+cmake -DCMAKE_PREFIX_PATH=/home/cgilab/pytorch/torch/lib/tmp_install ..
+make
+```
+
+- test your program
+
+``classifier <path-to-exported-script-module> <path-to-lable-file>``
+
+```
+> ./classifier ../resnet50.pt ../label.txt
+== Switch to GPU mode
+== ResNet50 loaded!
+== Label loaded! Let's try it
+== Input image path: [enter Q to exit]
+../pic/dog.jpg
+== image size: [976 x 549] ==
+== simply resize: [224 x 224] ==
+    ============= Top-1 =============
+    Label:  beagle
+    With Probability:  99.1228%
+    ============= Top-2 =============
+    Label:  Walker hound, Walker foxhound
+    With Probability:  0.469356%
+    ============= Top-3 =============
+    Label:  English foxhound
+    With Probability:  0.110916%
+== Input image path: [enter Q to exit]
+```
+![](./pic/dog.jpg)
+
+```
+../pic/shark.jpg
+== image size: [800 x 500] ==
+== simply resize: [224 x 224] ==
+    ============= Top-1 =============
+    Label:  tiger shark, Galeocerdo cuvieri
+    With Probability:  92.2599%
+    ============= Top-2 =============
+    Label:  great white shark, white shark, man-eater, man-eating shark
+    With Probability:  5.94252%
+    ============= Top-3 =============
+    Label:  hammerhead, hammerhead shark
+    With Probability:  1.77418%
+== Input image path: [enter Q to exit]
+Q
+```
+![](./pic/shark.jpg)
+
+
+Take it easy!!
---
+++ prediction.cpp
@@ -0,0 +1,113 @@
+// One-stop header.
+#include <torch/script.h>
+
+// headers for opencv
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#define kIMAGE_SIZE 224
+#define kCHANNELS 3
+#define kTOP_K 3
+
+bool LoadImage(std::string file_name, cv::Mat &image) {
+  image = cv::imread(file_name);  // CV_8UC3
+  if (image.empty() || !image.data) {
+    return false;
+  }
+  cv::cvtColor(image, image, CV_BGR2RGB);
+  std::cout << "== image size: " << image.size() << " ==" << std::endl;
+
+  // scale image to fit
+  cv::Size scale(kIMAGE_SIZE, kIMAGE_SIZE);
+  cv::resize(image, image, scale);
+  std::cout << "== simply resize: " << image.size() << " ==" << std::endl;
+
+  // convert [unsigned int] to [float]
+  image.convertTo(image, CV_32FC3, 1.0f / 255.0f);
+
+  return true;
+}
+
+bool LoadImageNetLabel(std::string file_name,
+                       std::vector<std::string> &labels) {
+  std::ifstream ifs(file_name);
+  if (!ifs) {
+    return false;
+  }
+  std::string line;
+  while (std::getline(ifs, line)) {
+    labels.push_back(line);
+  }
+  return true;
+}
+
+int main(int argc, const char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: classifier <path-to-exported-script-module> "
+                 "<path-to-lable-file>"
+              << std::endl;
+    return -1;
+  }
+
+  std::shared_ptr<torch::jit::script::Module> module =
+      torch::jit::load(argv[1]);
+  std::cout << "== Switch to GPU mode" << std::endl;
+  // to GPU
+  module->to(at::kCUDA);
+
+  assert(module != nullptr);
+  std::cout << "== ResNet50 loaded!\n";
+  std::vector<std::string> labels;
+  if (LoadImageNetLabel(argv[2], labels)) {
+    std::cout << "== Label loaded! Let's try it\n";
+  } else {
+    std::cerr << "Please check your label file path." << std::endl;
+    return -1;
+  }
+
+  std::string file_name = "";
+  cv::Mat image;
+  while (true) {
+    std::cout << "== Input image path: [enter Q to exit]" << std::endl;
+    std::cin >> file_name;
+    if (file_name == "Q") {
+      break;
+    }
+    if (LoadImage(file_name, image)) {
+      auto input_tensor = torch::from_blob(
+          image.data, {1, kIMAGE_SIZE, kIMAGE_SIZE, kCHANNELS});
+      input_tensor = input_tensor.permute({0, 3, 1, 2});
+      input_tensor[0][0] = input_tensor[0][0].sub_(0.485).div_(0.229);
+      input_tensor[0][1] = input_tensor[0][1].sub_(0.456).div_(0.224);
+      input_tensor[0][2] = input_tensor[0][2].sub_(0.406).div_(0.225);
+
+      // to GPU
+      input_tensor = input_tensor.to(at::kCUDA);
+
+      torch::Tensor out_tensor = module->forward({input_tensor}).toTensor();
+
+      auto results = out_tensor.sort(-1, true);
+      auto softmaxs = std::get<0>(results)[0].softmax(0);
+      auto indexs = std::get<1>(results)[0];
+
+      for (int i = 0; i < kTOP_K; ++i) {
+        auto idx = indexs[i].item<int>();
+        std::cout << "    ============= Top-" << i + 1
+                  << " =============" << std::endl;
+        std::cout << "    Label:  " << labels[idx] << std::endl;
+        std::cout << "    With Probability:  "
+                  << softmaxs[i].item<float>() * 100.0f << "%" << std::endl;
+      }
+
+    } else {
+      std::cout << "Can't load the image, please check your path." << std::endl;
+    }
+  }
+}
